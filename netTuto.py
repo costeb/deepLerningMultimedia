9bdcfb5d4115b7adea2337d0f61b260b8cdbd898
Benjamin <benjamincoste@outlook.fr>
2020-03-18 10:09:07 +0100

    Réduction taille convolution

--- netTuto.py
+++ netTuto.py
@@ -74,9 +74,9 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv1 = nn.Conv2d(3, 5, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(5, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
